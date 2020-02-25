3b972c6ef0ea562a5932f5e9dbc997f0ce212b79
Ubuntu <alina@project.qp5g0cxs4phelfdytvtcf4kxph.zx.internal.cloudapp.net>
2020-02-25 15:59:51 +0000

    First 2 pages added

--- flask-app/application/routes.py
+++ flask-app/application/routes.py
@@ -5,3 +5,6 @@ from application import app
 @app.route('/home')
 def home():
     return render_template('home.html', title='Home')
+@app.route('/products')
+def products():
+    return render_template('products.html', title='Products')
