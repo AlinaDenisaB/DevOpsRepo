8466661ad099f33d58010ea41d50fca8a57afb2f
Ubuntu <alina@vm.j2biiuacnyieddmeujeal5w5xc.zx.internal.cloudapp.net>
2020-02-29 18:08:54 +0000

    Add to cart buttons added

--- flask-app/application/routes.py
+++ flask-app/application/routes.py
@@ -8,3 +8,9 @@ def home():
 @app.route('/products')
 def products():
     return render_template('products.html', title='Products')
+@app.route('/cart')
+def cart():
+    return render_template('cart.html', title='Cart')
+@app.route('/admin')
+def admin():
+    return render_template('admin.html', title="Admin")
